e4d0e3c3fe0a06eba9ca7bfa98f4292227ac9ac6
KARAN KUMAR <kkvmalhi@gmail.com>
2023-12-21 13:16:16 +0500

    Environment Set

---
+++ list/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+//Components
+import Header from './components/Header/header'
+import Items from './components/Items/items'
+
+function App() {
+  return (
+    <div>
+     <Header />
+     <Items />
+    </div>
+  );
+}
+
+export default App;
---
+++ list/src/components/Items/items.jsx
@@ -0,0 +1,11 @@
+import './items.css'
+
+const Items = () => {
+    return ( 
+        <div>
+            <h2>Items</h2>
+        </div>
+     );
+}
+ 
+export default Items;
